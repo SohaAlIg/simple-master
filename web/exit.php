775c3b93b26c042dd6d4c08460ed5353aa05a921
Alexey Los <losai@yandex.ru>
2023-12-11 10:51:16 +0300

    Добавлены коментарии к отладке

--- web/exit.php
+++ web/exit.php
@@ -1,19 +1,18 @@
 <?php
     declare(strict_types=1);
-    echo "exit.php" . '<br>';
+    // echo "exit.php" . '<br>';
 
     session_start();
-    echo 'ID сессии: ' . session_id() . '<br>'; // идентификатор сессии
+    // echo 'ID сессии: ' . session_id() . '<br>'; // идентификатор сессии
 
-    echo '<pre>';
-    print_r($_SESSION);
-    echo '</pre>' . '<br>';
+    // echo '<pre>';
+    // print_r($_SESSION);
+    // echo '</pre>' . '<br>';
 
     if (isset($_SESSION['name'])) { 
         echo "Удаление сессии" . '<br>'; 
         session_destroy(); //Удаление сессии
     }
 
-    echo 'exit.php -> index.php' . '<br>';
-    sleep(2);
+    // echo 'exit.php -> index.php' . '<br>';
     header("Location: index.php");
--- web/post.php
+++ web/post.php
@@ -1,29 +1,29 @@
 <?php
     declare(strict_types=1);
 
-    echo "post.php" . '<br>';
+    // echo "post.php" . '<br>';
     session_start();
-    echo 'ID сессии: ' . session_id() . '<br>'; // идентификатор сессии
+    // echo 'ID сессии: ' . session_id() . '<br>'; // идентификатор сессии
 
-    echo '<pre>';
-    echo '$_SESSION: <br>';
-    print_r($_SESSION);
-    echo '$_POST: <br>';
-    print_r($_POST);
-    echo '</pre>';
+    // echo '<pre>';
+    // echo '$_SESSION: <br>';
+    // print_r($_SESSION);
+    // echo '$_POST: <br>';
+    // print_r($_POST);
+    // echo '</pre>';
 
 	if (isset($_POST['name'])) {
         $_SESSION['name'] = $_POST['name'];
     } 
 
-    echo '<pre>';
-    echo '$_SESSION: <br>';
-    print_r($_SESSION);
-    echo '$_POST: <br>';
-    print_r($_POST);
-    echo '</pre>';
+    // echo '<pre>';
+    // echo '$_SESSION: <br>';
+    // print_r($_SESSION);
+    // echo '$_POST: <br>';
+    // print_r($_POST);
+    // echo '</pre>';
     
-    echo 'post.php -> index.php' . PHP_EOL;
+    // echo 'post.php -> index.php' . PHP_EOL;
     header("Location: index.php");
 
     //var_dump($_FILES);
